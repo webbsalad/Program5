643d3b7c07966225763852e072b9f1d38930b748
Artyom <89246164+fuquyoma@users.noreply.github.com>
2024-09-17 18:29:32 +0300

    Delete ЛР№ 3 directory

--- ЛР№ 3/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='open-weather_API',
-      version='0.0',
-      description='Openweather API',
-      packages=['open-weather_API'],
-      author_email='gnevnov1996@mail.ru',
-      zip_safe=False)
