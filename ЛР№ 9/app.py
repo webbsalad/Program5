6fdf559f8b7ee8b6d97bbf190e8626515df56055
webbsalad <webbbsalad@gmail.com>
2024-12-17 13:35:44 +0300

    feat: better project for awsome man

--- ЛР№ 9/app.py
+++ ЛР№ 9/app.py
@@ -1,10 +1,11 @@
+# app.py
 from flask import Flask
-from auth import auth_blueprint
-from users import users_blueprint
+from router import router
 
 app = Flask(__name__)
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(users_blueprint)
+
+# Регистрация Blueprint
+app.register_blueprint(router)
 
 if __name__ == '__main__':
     app.run(debug=True)
